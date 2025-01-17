8e741bce14a93338daf690f5fda635d151a1ae33
moolraaj <raaj73906@gmail.com>
2025-01-17 23:16:20 +0530

    updated code

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   <h1>hii my name is ----  {process.env.NEXT_PUBLIC_USER_NAME}</h1>
   <h1>hii my email is ----  {process.env.NEXT_PUBLIC_USER_EMAIL}</h1>
   <h1>hii my password is ----  {process.env.NEXT_PUBLIC_USER_PASS}</h1>
-  <p>final touch deployment is end</p>
+
     </>
   );
 }
